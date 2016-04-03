434521dae1570d895d47ea27eea9495dcb508cad
majeed21 <majeedmohammadian@gmail.com>
2016-04-04 01:31:12 +0430

    add admin controller class and template audio

---
+++ admin/controller/common/audio.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/common/audio.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerCommonAudio extends adminController
+{
+    public function index()
+    {
+        global $client;
+    
+        $data['site_url'] = factory::getConfig()->get('url');
+        
+        return loaderTemplate('common/audio', $data, $client);
+    }
+}
+?>
---
+++ admin/templates/common/audio.tpl
@@ -0,0 +1,4 @@
+<!-- START PRELOADS -->
+<audio id="audio-alert" src="<?php echo $site_url; ?>templates/backend/audio/alert.mp3" preload="auto"></audio>
+<audio id="audio-fail" src="<?php echo $site_url; ?>templates/backend/audio/fail.mp3" preload="auto"></audio>
+<!-- END PRELOADS -->
