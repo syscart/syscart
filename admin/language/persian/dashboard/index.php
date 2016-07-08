b209e7cc97a7bad4a7a76406a4cc5d6522f61e31
majeed21 <majeedmohammadian@gmail.com>
2016-07-08 18:03:10 +0430

    modify language admin dashboard

--- admin/language/persian/dashboard/index.php
+++ admin/language/persian/dashboard/index.php
@@ -8,7 +8,6 @@ $_['text_log_in']              = 'ورود';
 $_['text_login_title']         = 'ورود به فروشگاه';
 $_['text_username']            = 'نام کاربری';
 $_['text_password']            = 'رمز عبور';
-$_['text_or']                  = 'یا';
 $_['text_login_google']        = 'ورود با گوگل';
 $_['text_do_not_have_account'] = 'آیا حساب کاربری ندارید؟';
 $_['text_create_an_account']   = 'ساخت حساب کاربری';
