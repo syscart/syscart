9236d511201be5e41a09bead42b1677849d24e21
majeed21 <majeedmohammadian@gmail.com>
2016-07-08 17:55:02 +0430

    add admin controller common footer

---
+++ admin/controller/common/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/common/footer.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerCommonFooter extends adminController
+{
+    public function index()
+    {
+        global $client;
+        $language = loaderLanguage('common/footer', $client);
+        
+        foreach($language as $item => $value) {
+            $data[$item] = $value;
+        }
+        
+        $data['site_url'] = factory::getConfig()->get('url');
+        
+        return loaderTemplate('common/footer', $data, $client);
+    }
+}
+?>
