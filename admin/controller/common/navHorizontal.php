b2c01b7e54a9ad6deff96f6752a97ae2d323e7eb
majeed21 <majeedmohammadian@gmail.com>
2016-07-23 00:53:47 +0430

    create admin controller navHorizontal

---
+++ admin/controller/common/navHorizontal.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/common/navHorizontal.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerCommonNavHorizontal extends adminController
+{
+    public function index()
+    {
+        global $client, $sysConfig;
+
+        $data['site_url'] = $sysConfig->get('url');
+    
+        $data['tasks'] = loaderController('common'.DS.'tasks', 'index', $client);
+        $data['messages'] = loaderController('common'.DS.'messages', 'index', $client);
+        
+        return loaderTemplate('common/navHorizontal', $data, $client);
+    }
+}
---
+++ admin/templates/common/navHorizontal.tpl
@@ -0,0 +1,18 @@
+<ul class="x-navigation x-navigation-horizontal x-navigation-panel">
+    <li class="xn-icon-button">
+        <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>
+    </li>
+    
+    <li class="xn-search">
+        <form role="form">
+            <input type="text" name="search" placeholder="{{t:general.search}}"/>
+        </form>
+    </li>
+    
+    <li class="xn-icon-button pull-right">
+        <a href="#" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
+    </li>
+    
+    <?php echo $messages; ?>
+    <?php echo $tasks; ?>
+</ul>
