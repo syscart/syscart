53d441e5f039701f10465e8de720f85f68b83999
majeed21 <majeedmohammadian@gmail.com>
2016-07-23 00:22:54 +0430

    create tasks in header controller

---
+++ admin/controller/common/tasks.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/common/tasks.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerCommonTasks extends adminController
+{
+    public function index()
+    {
+        global $client, $sysConfig;
+
+        $data['site_url'] = $sysConfig->get('url');
+        
+        return loaderTemplate('common/tasks', $data, $client);
+    }
+}
