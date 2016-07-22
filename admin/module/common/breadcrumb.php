554adb74c8dbc9ada46decc0626346658b769762
majeed21 <majeedmohammadian@gmail.com>
2016-07-22 22:04:58 +0430

    create module admin common breadcrumb

---
+++ admin/module/common/breadcrumb.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/module/common/breadcrumb.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminModuleCommonBreadcrumb
+{
+    public function render($breadcrumb = array())
+    {
+        $data = '<!-- START BREADCRUMB -->';
+        if($breadcrumb) {
+            $data .= '<ul class="breadcrumb">';
+            foreach( $breadcrumb as $item ) {
+                if(isset( $item['url'] ))
+                    $data .= '<li><a href="'.$item['url'].'">'.$item['text'].'</a></li>';
+                else
+                    $data .= '<li>'.$item['text'].'</li>';
+            }
+            $data .= '</ul>';
+        }
+        $data .= '<!-- END BREADCRUMB -->';
+        
+        return $data;
+    }
+}
