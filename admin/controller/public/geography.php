a4b135f2175dfa8644707304f0af8b3e4096ecf6
majeed21 <majeedmohammadian@gmail.com>
2016-07-29 19:10:44 +0430

    add public geography in admin controller

---
+++ admin/controller/public/geography.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/public/geography.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerPublicGeography extends adminController
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+    
+    public function getZoneSelect()
+    {
+        global $client, $sysDoc;
+
+        $data['success'] = false;
+
+        $countryId = platformRequest::getVar('countryId');
+        if($countryId) {
+            $geographyObject = loaderModule('local'.DS.'geography', $client);
+
+            $data['success'] = true;
+            $zones = $geographyObject->getZoneCountrySelectBox($countryId);
+
+            $data['data'] = '<option value="-1">{{t:general.please_select}}</option>';
+            foreach( $zones as $zone ) {
+                $name = ($zone['faName'] != '') ? $zone['faName'] : $zone['enName'];
+                $data['data'] .= '<option value="'.$zone['id'].'">'.$name.'</option>';
+            }
+        } else {
+            $data['error'] = '{{t:adminSetting.error_not_set_country}}';
+        }
+
+        $sysDoc->setBody($data);
+        $sysDoc->renderJson();
+    }
+
+    public function getCitySelect()
+    {
+        global $client, $sysDoc;
+
+        $data['success'] = false;
+
+        $countryId = platformRequest::getVar('countryId');
+        if($countryId) {
+            $zoneId = platformRequest::getVar('zoneId');
+            if($zoneId) {
+                $geographyObject = loaderModule('local'.DS.'geography', $client);
+                $data[ 'success' ] = true;
+                $cities = $geographyObject->getCityZoneCountrySelectBox($countryId, $zoneId);
+                $data[ 'data' ] = '<option value="-1">{{t:general.please_select}}</option>';
+                foreach( $cities as $city ) {
+                    $name = ( $city[ 'faName' ] != '' ) ? $city[ 'faName' ] : $city[ 'enName' ];
+                    $data[ 'data' ] .= '<option value="'.$city[ 'id' ].'">'.$name.'</option>';
+                }
+            } else
+                $data['error'] = '{{t:adminSetting.error_not_set_zone}}';
+        } else
+            $data['error'] = '{{t:adminSetting.error_not_set_country}}';
+
+        $sysDoc->setBody($data);
+        $sysDoc->renderJson();
+    }
+}
