c411d247ea89fc099d2847a20a9b9045dd9b340f
majeed21 <majeedmohammadian@gmail.com>
2016-08-12 14:56:26 +0430

    delete language file

--- admin/language/persian/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_['text_yes']      = 'بله';
-$_['text_no']       = 'خیر';
-
-$_['text_enable']   = 'فعال';
-$_['text_disable']  = 'غیر فعال';
-
-$_['text_or']       = 'یا';
-$_['text_and']      = 'و';
