c0e0ff9161a4e89dd48fc54f09c9ad027ee51a55
majeed21 <majeedmohammadian@gmail.com>
2016-08-16 20:31:26 +0430

    change address script jquery and jquery ui

--- libraries/html/adminLoadDefault.php
+++ libraries/html/adminLoadDefault.php
@@ -80,8 +80,8 @@ class htmlAdminLoadDefault
     public function scriptFooter()
     {
         global $client, $sysDocScript;
-        $sysDocScript->setFooter('media/js/jquery/script/core/2.1.4/jquery.js');
-        $sysDocScript->setFooter('media/js/jquery/script/ui/1.11.4/jquery-ui.min.js');
+        $sysDocScript->setFooter('media/js/jquery/script/core/3.1.0/jquery.js');
+        $sysDocScript->setFooter('media/js/jquery/script/ui/1.12.0/jquery-ui.min.js');
         $sysDocScript->setFooter('templates/backend/js/plugins/bootstrap/bootstrap.min.js');
         $sysDocScript->setFooter('templates/backend/js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js');
         $sysDocScript->setFooter('templates/backend/js/plugins.js');
