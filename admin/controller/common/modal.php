2abc2593fd980814c736f804c1250d638e6c0ca2
majeed21 <majeedmohammadian@gmail.com>
2016-11-13 00:57:48 +0330

    افزودن کنترلر modal در ادمین

---
+++ admin/controller/common/modal.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package    system cart
+ *             admin/controller/common/modal.php
+ *
+ * @copyright  Copyright (C) 2016 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @company    syscart
+ * @autor      majeed mohammadian
+ */
+
+defined('syscart') or die('access denied...!');
+
+class adminControllerCommonModal extends adminController
+{
+    public function index()
+    {
+        return null;
+    }
+
+    public function render($option = [])
+    {
+        global $client;
+
+        $data = [];
+        if(isset($option['id']))
+            $data['id'] = $option['id'];
+        else {
+            $randomId = 'modal-'.rand(100,999);
+            $data['id'] = $randomId;
+        }
+        
+        if(isset($option['size']))
+            if($option['size'] == 'large')
+                $data['size'] = 'modal-lg';
+            elseif($option['size'] == 'small')
+                $data['size'] = 'modal-sm';
+            else
+                $data['size'] = '';
+        else
+            $data['size'] = '';
+
+        if(isset($option['buttonFooter']))
+            foreach( $option['buttonFooter'] as $item => $button ) {
+                $data['buttonFooter'][$item]['title'] = $button['title'];
+                $data['buttonFooter'][$item]['size'] = (isset($button['size'])) ? ' btn-'.$button['size'] : '';
+                $data['buttonFooter'][$item]['color'] = (isset($button['color'])) ? ' btn-'.$button['size'] : ' btn-default';
+                $data['buttonFooter'][$item]['close'] = (isset($button['close'])) ? ' data-dismiss="modal"' : '';
+            }
+        else
+            $data['buttonFooter'] = '';
+        
+        $data['showHeader'] = (isset($option['showHeader'])) ? $option['showHeader'] : true;
+        $data['showFooter'] = (isset($option['showFooter'])) ? $option['showFooter'] : true;
+        $data['titleId'] = 'modal-title-'.rand(100,999);
+        $data['title'] = (isset($option['title'])) ? $option['title'] : '';
+        $data['body'] = (isset($option['body'])) ? $option['body'] : '';
+        
+        return loaderTemplate('common/modal', $data, $client);
+    }
+}
---
+++ admin/templates/common/modal.tpl
@@ -0,0 +1,24 @@
+<!-- MODALS -->
+<div class="modal" id="<?=$id?>" tabindex="-1" role="dialog" aria-labelledby="<?=$titleId?>" aria-hidden="true">
+    <div class="modal-dialog<?=$size?>">
+        <div class="modal-content">
+            <?php if($showHeader) { ?>
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span>
+                    <span class="sr-only">{{t:commonModal.close}}</span>
+                </button>
+                <h4 class="modal-title" id="<?=$titleId?>"><?=$title?></h4>
+            </div>
+            <?php } ?>
+            <div class="modal-body"><?=$body?></div>
+            <?php if($showHeader) { ?>
+            <div class="modal-footer">
+                <?php foreach( $buttonFooter as $item => $button ) { ?>
+                    <button type="button" class="btn<?=$button['size']?><?=$button['color']?>"<?=$button['close']?>><?=$button['title']?></button>
+                <?php } ?>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
